b69e4c35030ff2be87089f1b0290ed39f1ff60dc
Vassilios <vogiatzisvassilios@gmail.com>
2019-06-24 23:14:41 +0300

    Updated functions.

--- cogs/fakemon.py
+++ cogs/fakemon.py
@@ -10,3 +10,8 @@ async def get_random_fakemon(database):
 async def get_fakemon(database, fakemon):
     row = await database.fetchrow("SELECT * FROM allfakemon WHERE name = $1", fakemon.capitalize())
     return row
+
+
+async def get_fakemon_information(database, fakemon_id):
+    row = await database.fetchrow("SELECT * FROM ownedfakemon WHERE fakemonid = $1", fakemon_id)
+    return row

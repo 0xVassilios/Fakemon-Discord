06dcd9300870eb48c5ff444a2b07243cfd42551e
Vassilios <vogiatzisvassilios@gmail.com>
2019-06-25 12:34:14 +0300

    New cog.

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def run():
         await bot.logout()
 
 initial_extensions = ['cogs.pokedex', 'cogs.starters',
-                      'cogs.adventures', 'cogs.locations', 'cogs.wildfakemon']
+                      'cogs.adventures', 'cogs.locations', 'cogs.wildfakemon', 'cogs.inventory', 'cogs.shop']
 
 if __name__ == '__main__':
     for extension in initial_extensions:
